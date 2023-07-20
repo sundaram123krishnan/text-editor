4422aec7d1a9b4f02456bc6441728129a5acb52e
sundaram123krishnan <krishnsundaram@gmail.com>
2023-07-20 15:13:54 +0530

    made standard output into raw terminal mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod editor;
+use editor::check_input;
+use std::io::stdout;
+use termion::raw::IntoRawMode;
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+    check_input();
 }
