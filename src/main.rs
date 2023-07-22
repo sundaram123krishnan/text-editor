dfc5815db5e82e2f5db1cab99f2a0e7db13c1ce7
sundaram123krishnan <krishnsundaram@gmail.com>
2023-07-22 23:52:10 +0530

    importing editor module and using in main function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod editor;
-use editor::check_input;
-use std::io::stdout;
-use termion::raw::IntoRawMode;
+use crate::editor::read_keys;
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    check_input();
+    read_keys();
 }
