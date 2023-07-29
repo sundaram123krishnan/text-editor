22ce1be5e1b3453915abe5745bfb9a77ac2db395
sundaram123krishnan <krishnsundaram@gmail.com>
2023-07-29 23:18:13 +0530

    refactoring codes and fixing line issues

--- src/editor.rs
+++ src/editor.rs
@@ -1,37 +1,9 @@
 use colored::*;
-use std::fs::read;
+use crossterm::cursor;
 use std::io::{stdin, stdout, Write};
 use std::process::exit;
 use terminal_size::{terminal_size, Height, Width};
 use termion::{event::Key, input::TermRead, raw::IntoRawMode};
-
-fn quit() -> Key {
-    Key::Ctrl('q')
-}
-
-fn enter() -> Key {
-    Key::Char('\n')
-}
-
-fn up_arrow() -> Key {
-    Key::Up
-}
-
-fn left_arrow() -> Key {
-    Key::Left
-}
-
-fn right_arrow() -> Key {
-    Key::Right
-}
-
-fn down_arrow() -> Key {
-    Key::Down
-}
-
-fn backspace() -> Key {
-    Key::Backspace
-}
 
 fn get_terminal_size() -> (u16, u16) {
     let size = terminal_size();
@@ -44,7 +16,13 @@ fn get_terminal_size() -> (u16, u16) {
 }
 
 fn tilde() {
-    print!("{} {}", termion::clear::All, termion::cursor::Goto(1, 1));
+    write!(
+        stdout(),
+        "{} {}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    )
+    .unwrap();
 
     let (_w, h) = get_terminal_size();
 
@@ -53,86 +31,69 @@ fn tilde() {
     }
 }
 
-pub fn print_file(file_name: &mut String) {
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    // print!("{} {}", termion::clear::All, termion::cursor::Goto(1, 1));
-    let mut j = 1;
-
-    tilde();
-    for i in file_name.lines() {
-        write!(stdout, "{} {i}", termion::cursor::Goto(3, j)).unwrap();
-        j += 1;
-        write!(stdout, "\r").unwrap();
-    }
-    read_keys();
-    stdout.flush().unwrap();
-}
+// pub fn print_file(file_name: &mut String) {
+//     let mut stdout = stdout().into_raw_mode().unwrap();
+//     tilde();
+//     for i in file_name.lines() {
+//         write!(stdout, "{} {i}", crossterm::cursor::MoveTo(2, col)).unwrap();
+//         write!(stdout, "\r").unwrap();
+//     }
+//     read_keys();
+//     stdout.flush().unwrap();
+// }
 
 pub fn read_keys() {
     let mut stdout = stdout().into_raw_mode().unwrap();
-    let mut j = 2;
     let (w, _h) = get_terminal_size();
-    let i = w;
-    // tilde();
-    print!("{}", termion::cursor::Goto(3, 1));
-    let mut shadow_i = 3;
+    tilde();
+    print!("{}", cursor::MoveTo(2, 0));
 
     for c in stdin().keys() {
+        let (row, col) = cursor::position().unwrap();
+
         let c = c.unwrap_or_else(|err| {
             println!("Error parsin keys {}", err);
             exit(1);
         });
 
-        let q = quit();
-        let e = enter();
-        let up_arrow = up_arrow();
-        let down_arrow = down_arrow();
-        let left_arrow = left_arrow();
-        let right_arrow = right_arrow();
-        let backspace = backspace();
-
-        if c == q {
-            exit(1);
-        } else if c == e {
-            write!(stdout, "{}", termion::cursor::Goto(3, j)).unwrap();
-            j += 1;
-            shadow_i = 4;
-        } else if c == up_arrow {
-            write!(stdout, "{}", termion::cursor::Up(1)).unwrap();
-            j -= 1;
-        } else if c == down_arrow {
-            write!(stdout, "{}", termion::cursor::Down(1)).unwrap();
-            j += 1;
-        } else if c == left_arrow {
-            if shadow_i <= 3 {
-                write!(stdout, "{}", termion::cursor::Up(1)).unwrap();
-            } else {
-                write!(stdout, "{}", termion::cursor::Left(1)).unwrap();
-                shadow_i -= 1;
+        match c {
+            Key::Char('\n') => {
+                write!(stdout, "{}", cursor::MoveTo(2, col + 1)).unwrap();
             }
-        } else if c == right_arrow {
-            write!(stdout, "{}", termion::cursor::Right(1)).unwrap();
-        } else if c == backspace {
-            write!(stdout, "{}", termion::cursor::Left(1)).unwrap();
-            write!(stdout, " ").unwrap();
-            write!(stdout, "{}", termion::cursor::Left(1)).unwrap();
-        } else {
-            let c = match c {
-                Key::Char(c) => c,
-                _ => 'q',
-            };
-            if shadow_i <= i {
-                write!(stdout, "{c}").unwrap();
-                write!(stdout, "{}", termion::cursor::BlinkingUnderline).unwrap();
-                shadow_i += 1;
-            } else {
-                write!(stdout, "{}", termion::cursor::Goto(3, j)).unwrap();
-                write!(stdout, "{c}").unwrap();
-                shadow_i = 4;
-                j += 1;
+            Key::Char(c) => {
+                if row <= w - 2 {
+                    write!(stdout, "{c}").unwrap();
+                    write!(stdout, "{}", cursor::EnableBlinking).unwrap();
+                } else {
+                    write!(stdout, "{}", cursor::MoveTo(2, col + 1)).unwrap();
+                    write!(stdout, "{c}").unwrap();
+                }
+            }
+            Key::Up => {
+                write!(stdout, "{}", cursor::MoveUp(1)).unwrap();
+            }
+            Key::Down => {
+                write!(stdout, "{}", cursor::MoveDown(1)).unwrap();
+            }
+            Key::Left => {
+                write!(stdout, "{}", cursor::MoveLeft(1)).unwrap();
+            }
+            Key::Right => {
+                write!(stdout, "{}", cursor::MoveRight(1)).unwrap();
+            }
+            Key::Backspace => {
+                write!(stdout, "{}", cursor::MoveLeft(1)).unwrap();
+                write!(stdout, " ").unwrap();
+                write!(stdout, "{}", cursor::MoveLeft(1)).unwrap();
+            }
+            Key::Ctrl('q') => {
+                break;
+            }
+
+            _other => {
+                write!(stdout, "other").unwrap();
             }
         }
-
         stdout.flush().unwrap();
     }
 }
