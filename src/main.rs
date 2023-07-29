05011ce97f1584ea17e62dd749996f0edf4fb4a4
sundaram123krishnan <krishnsundaram@gmail.com>
2023-07-29 23:18:38 +0530

    added alternate screen for display

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
 mod editor;
-use crate::editor::print_file;
+// use crate::editor::print_file;
 use crate::editor::read_keys;
-use std::fs;
-use std::process;
-use std::{env, process::exit};
+// use std::fs;
+// use std::process;
+// use std::{env, process::exit};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-
-    if args.len() == 1 {
-        read_keys();
-    } else if args.len() == 2 {
-        let input = &args[1];
-
-        let mut contents = fs::read_to_string(&input).unwrap_or_else(|_err| {
-            println!("Error reading file");
-            process::exit(5);
-        });
-        println!("{}", contents);
-        print_file(&mut contents);
-    } else {
-        println!("Too many arguments provided");
-        exit(1);
-    }
+use crossterm::{
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+    Result,
+};
+use std::io::stdout;
+fn main() -> Result<()> {
+    // let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    //
+    // if args.len() == 1 {
+    //     read_keys();
+    // } else if args.len() == 2 {
+    //     let input = &args[1];
+    //
+    //     let mut contents = fs::read_to_string(&input).unwrap_or_else(|_err| {
+    //         println!("Error reading file");
+    //         process::exit(5);
+    //     });
+    //     println!("{}", contents);
+    //     print_file(&mut contents);
+    // } else {
+    //     println!("Too many arguments provided");
+    //     exit(1);
+    // }
+    execute!(stdout(), EnterAlternateScreen)?;
+    read_keys();
+    execute!(stdout(), LeaveAlternateScreen)
 }
