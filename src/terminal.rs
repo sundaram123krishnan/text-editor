c106eed18247ec8473366b2e929a0daf260399ce
sundaram123krishnan <krishnsundaram@gmail.com>
2023-08-02 21:15:00 +0530

    placing all terminal related interface

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,8 @@
+use std::io::{self, stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
@@ -5,6 +10,7 @@ pub struct Size {
 
 pub struct Terminal {
     size: Size,
+    stdout: RawTerminal<io::Stdout>,
 }
 
 impl Terminal {
@@ -16,9 +22,29 @@ impl Terminal {
                 width: x,
                 height: y,
             },
+            stdout: stdout().into_raw_mode().unwrap(),
         })
     }
+
     pub fn size(&self) -> &Size {
         &self.size
+    }
+    pub fn read_key() -> Key {
+        loop {
+            let key = stdin().keys().next().unwrap();
+            return key.unwrap();
+        }
+    }
+    pub fn clear_screen(&mut self) {
+        write!(self.stdout, "{}", termion::clear::All).unwrap();
+    }
+
+    pub fn cursor_position(&mut self, w: u16, h: u16) {
+        let w = w.saturating_add(1);
+        let h = h.saturating_add(1);
+        write!(self.stdout, "{}", termion::cursor::Goto(w, h)).unwrap();
+    }
+    pub fn flush_output(&mut self) {
+        self.stdout.flush().unwrap();
     }
 }
