807249c1469e33a9cbfd2f93fa13529623782924
sundaram123krishnan <krishnsundaram@gmail.com>
2023-08-04 23:42:28 +0530

    added welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -9,10 +9,22 @@ pub struct Editor {
 
 impl Editor {
     pub fn run(&mut self) {
+        let mut ok: bool = false;
         self.terminal.clear_screen();
         self.tilde();
+        self.terminal.start();
         self.terminal.cursor_position(2, 0);
         loop {
+            if ok == false {
+                let size = self.terminal.size();
+                let display_h = size.height / 3;
+                let display_w = size.width / 3;
+                self.terminal.cursor_position(display_w, display_h);
+                print!("{}", termion::clear::CurrentLine);
+                ok = true;
+                self.terminal.cursor_position(2, 0);
+            }
+
             let pressed_key = self.process_keys();
 
             if self.quit == true {
@@ -35,6 +47,13 @@ impl Editor {
         for _ in 0..size.height {
             println!("~\r");
         }
+
+        let display_h = size.height / 3;
+        let display_w = size.width / 4;
+
+        self.terminal.cursor_position(display_w, display_h);
+        println!("Welcome to LinNote editor -- Version 0.0.1. Create, Edit and Delete");
+        self.terminal.cursor_position(2, 0);
     }
 
     fn process_keys(&mut self) -> Option<char> {
