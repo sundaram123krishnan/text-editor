2a0897fe06cb1763a49885bd9469ff1ef16fa957
sundaram123krishnan <krishnsundaram@gmail.com>
2023-08-06 17:24:05 +0530

    modifying cursor_position method

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,4 @@
+use crate::editor::Position;
 use std::io::{self, stdin, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
@@ -44,10 +45,12 @@ impl Terminal {
         write!(self.stdout, "{}", termion::clear::All).unwrap();
     }
 
-    pub fn cursor_position(&mut self, w: u16, h: u16) {
-        let w = w.saturating_add(1);
-        let h = h.saturating_add(1);
-        write!(self.stdout, "{}", termion::cursor::Goto(w, h)).unwrap();
+    pub fn cursor_position(&mut self, pos: &Position) {
+        let mut w = pos.x;
+        let mut h = pos.y;
+        w = w.saturating_add(1);
+        h = w.saturating_add(1);
+        write!(self.stdout, "{}", termion::cursor::Goto(w as u16, h as u16)).unwrap();
     }
     pub fn flush_output(&mut self) {
         self.stdout.flush().unwrap();
