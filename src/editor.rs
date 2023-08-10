4ca21aa147acde2801bec9817e51cb9853558fbb
sundaram123krishnan <krishnsundaram@gmail.com>
2023-08-10 19:35:13 +0530

    added cursor position struct

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,12 @@
-use crate::terminal::Terminal;
+use crate::terminal::{self, Terminal};
 use colored::Colorize;
 use std::io::Write;
 use std::process::exit;
 use termion::event::Key;
 
 pub struct Position {
-    pub x: usize,
-    pub y: usize,
+    x: usize,
+    y: usize,
 }
 
 pub struct Editor {
@@ -22,29 +22,100 @@ impl Editor {
         self.terminal.clear_screen();
         self.tilde();
         self.terminal.start();
-        self.terminal.cursor_position(&Position { x: 2, y: 0 });
+        self.terminal.cursor_position(2, 0);
+        let Position { mut x, mut y } = self.cursor_pos;
+        self.cursor_pos.x = 2;
+        self.cursor_pos.y = 0;
         loop {
             if ok == false {
                 self.clear_message();
                 ok = true;
-                self.terminal.cursor_position(&Position { x: 2, y: 0 });
-            }
-
-            let pressed_key = self.process_keys();
-
-            if self.quit == true {
-                exit(1);
+                self.terminal.cursor_position(2, 0);
             } else {
-                if let Some(_c) = pressed_key {
-                    let pressed_key = match pressed_key {
-                        Some(pressed_key) => pressed_key,
-                        None => exit(1),
-                    };
-                    print!("{pressed_key}");
+                let pressed_key = self.process_keys();
+
+                if self.quit == true {
+                    exit(1);
+                } else {
+                    if let Some(_c) = pressed_key {
+                        let pressed_key = match pressed_key {
+                            Some(pressed_key) => pressed_key,
+                            None => exit(1),
+                        };
+                        match pressed_key {
+                            Key::Char(c) => {
+                                print!("{c}");
+                                self.cursor_pos.x += 1;
+                            }
+
+                            Key::Right => {
+                                self.move_cursor_right();
+                            }
+
+                            Key::Left => {
+                                self.move_cursor_left();
+                            }
+
+                            Key::Up => {
+                                self.move_cursor_up();
+                            }
+
+                            Key::Down => {
+                                self.move_cursor_down();
+                            }
+                            _ => todo!(),
+                        }
+                    }
+                    self.terminal.flush_output();
                 }
-                self.terminal.flush_output();
             }
         }
+    }
+
+    fn move_cursor_right(&mut self) {
+        self.cursor_pos.x += 1;
+        write!(
+            self.terminal.stdout,
+            "{}",
+            termion::cursor::Goto(self.cursor_pos.x as u16, self.cursor_pos.y as u16)
+        )
+        .unwrap();
+    }
+
+    fn move_cursor_left(&mut self) {
+        self.cursor_pos.x -= 1;
+        if self.cursor_pos.x <= 2 {
+            self.cursor_pos.x = 2;
+        }
+        write!(
+            self.terminal.stdout,
+            "{}",
+            termion::cursor::Goto(self.cursor_pos.x as u16, self.cursor_pos.y as u16)
+        )
+        .unwrap();
+    }
+
+    fn move_cursor_down(&mut self) {
+        self.cursor_pos.y += 1;
+        write!(
+            self.terminal.stdout,
+            "{}",
+            termion::cursor::Goto(self.cursor_pos.x as u16, self.cursor_pos.y as u16)
+        )
+        .unwrap();
+    }
+
+    fn move_cursor_up(&mut self) {
+        self.cursor_pos.y -= 1;
+        if self.cursor_pos.y <= 0 {
+            self.cursor_pos.y = 0;
+        }
+        write!(
+            self.terminal.stdout,
+            "{}",
+            termion::cursor::Goto(self.cursor_pos.x as u16, self.cursor_pos.y as u16)
+        )
+        .unwrap();
     }
 
     fn tilde(&mut self) {
@@ -56,25 +127,28 @@ impl Editor {
         let display_h = size.height / 3;
         let display_w = size.width / 4;
 
-        self.terminal.cursor_position(&Position {
-            x: display_w as usize,
-            y: display_h as usize,
-        });
+        self.terminal
+            .cursor_position(display_w as usize, display_h as usize);
+
         let value = format!("{}", self.welcome_message);
         println!("{}", value.blue().bold());
 
-        self.terminal.cursor_position(&Position { x: 2, y: 0 });
+        self.terminal.cursor_position(2, 0);
     }
 
-    fn process_keys(&mut self) -> Option<char> {
+    fn process_keys(&mut self) -> Option<Key> {
         let key_pressed = Terminal::read_key();
         match key_pressed {
             termion::event::Key::Ctrl('q') => {
                 self.quit = true;
                 return None;
             }
-            Key::Char(c) => return Some(c),
-            _ => return Some('c'),
+            Key::Char(c) => return Some(Key::Char(c)),
+            Key::Right => {
+                return Some(Key::Right);
+            }
+
+            _ => return Some(Key::PageUp),
         }
     }
 
@@ -83,10 +157,8 @@ impl Editor {
         let display_h = size.height / 3;
         let display_w = size.width / 4;
         self.welcome_message.clear();
-        self.terminal.cursor_position(&Position {
-            x: display_w as usize,
-            y: display_h as usize,
-        });
+        self.terminal
+            .cursor_position(display_w as usize, display_h as usize);
         write!(
             self.terminal.stdout,
             "                                                                  "
