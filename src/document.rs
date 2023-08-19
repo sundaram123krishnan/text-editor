e88cfc4d29a29fd2a391853d26a98a2fdb52ea1b
sundaram123krishnan <krishnsundaram@gmail.com>
2023-08-19 23:51:54 +0530

    added file path to open method

--- src/document.rs
+++ src/document.rs
@@ -2,16 +2,19 @@ use std::fs;
 
 use crate::row::Row;
 
+#[derive(Default)]
 pub struct Document {
     pub rows: Vec<Row>,
 }
 
 impl Document {
-    pub fn open() -> Self {
+    pub fn open(filename: &str) -> Self {
         let mut rows = Vec::new();
-        let contents = fs::read_to_string("/home/sundaram/Desktop/cpp/main.cpp").unwrap();
+        let contents = fs::read_to_string(filename).unwrap();
         for i in contents.lines() {
-            rows.push(Row::slice(i));
+            rows.push(Row {
+                string: i.to_string(),
+            })
         }
         Self { rows }
     }
