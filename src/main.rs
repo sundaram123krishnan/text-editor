f47da2a4bb454337581078b37dcdcd34f5f01ead
sundaram123krishnan <krishnsundaram@gmail.com>
2023-08-20 18:40:47 +0530

    added alternate screen for display

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use crate::editor::Editor;
 mod document;
 mod row;
 mod terminal;
+use crossterm::{
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
 
-fn main() {
+fn main() -> io::Result<()> {
+    execute!(io::stdout(), EnterAlternateScreen)?;
     let mut editor = Editor::default();
     editor.run();
+    execute!(io::stdout(), LeaveAlternateScreen)
 }
